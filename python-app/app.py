7946cd5b8310b4ffad217b1792bd300368b8d646
Mart Noten <notenm@amazon.com>
2021-02-07 18:48:19 +0100

    Adding function to read SQS messages

--- python-app/app.py
+++ python-app/app.py
@@ -4,6 +4,25 @@ sqs = boto3.client('sqs')
 
 queue_url = 'https://sqs.eu-central-1.amazonaws.com/629373475343/DockerEcsSqsAutoScalingStack-DockerEcsSqsAutoScalingQueue2DAE4A36-5DMX2GTEINGG' # <-- Dont forget to change this to your SQS url
 
+def read_sqs():
+    print(f"We will try to read {queue_url}")
+    response = sqs.receive_message(
+        QueueUrl=queue_url,
+        AttributeNames=[
+            'SentTimestamp'
+        ],
+        MaxNumberOfMessages=1,
+        MessageAttributeNames=[
+            'All'
+        ],
+        VisibilityTimeout=0,
+        WaitTimeSeconds=0
+    )
+
+    print(f"SQS responded with {response}")
+
+    return response['Messages']
+
 def delete_sqs_message(receipt_handle):
     print(f"Deleting message {receipt_handle}")
     # Delete received message from queue
